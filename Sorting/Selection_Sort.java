dbc96c9d62d210ce5adf7b9914137d82a8aab9b6
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-07-31 05:09:07 +0000

    Add Expantion

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -41,6 +41,17 @@ public class Selection_Sort {
 /*
  * lt understand by the example given {13 , 15 , 18 , 25, 9 }
  * 
- * fi
+ * first understand why we set loop i = 0 ; i <= arr.length-2; i++
+ * let understand by follwing way
  * 
+ * 0    1    2    3    4 (index)
+ * 9  15   18   25    13 (step 1 which one number is minimun swap it to index 0 here is 9) 
+ * 9  13   18   25    15 (Step 2 in second iteration i is incrsea by 1 so j start from 1 and find minimun and swap to starting )
+ * 9  13   15   25    18  (Step 3 in this step j =2 so it start find the minimun ans swap)
+ * 9  13   15   18    25  (Step 4 in this step j =3 so it start find the minimun ans swap)
+ * 
+ * at the end we get sorted array so we se n-2 
+ * as per above explaination we does not need to traver array for length-1 which end 
+ * becouse our second array start from i 
+ * so we know left side is oredy sorted so no need to start fron index 0 
  */
