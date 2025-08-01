ef544a85a4187038776cd7c06ce08a123f9e17b6
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-01 05:44:22 +0000

    add Explantion of insertion sort

--- Sorting/Insertion_Sort.java
+++ Sorting/Insertion_Sort.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 
 public class Insertion_Sort {
 
-    
+    /*
+     Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct position in a sorted portion of the list.
+    It is like sorting playing cards in your hands.
+    You split the cards into two groups: the sorted cards and the unsorted cards. 
+    Then, you pick a card from the unsorted group and put it in the right place in the sorted group.
+     */
     
     public static void InsertionSort(int arr[]){
 
@@ -28,3 +33,14 @@ public class Insertion_Sort {
         System.out.println("After Sorting the Array:-" + Arrays.toString(arr));
     }
 }
+
+/*
+ * 
+ * Approach: 
+  1:- Select an element in each iteration from the unsorted array(using a loop).
+  2:-Place it in its corresponding position in the sorted part and shift the remaining elements accordingly (using an inner loop and swapping).
+  3:-The “inner while loop” basically shifts the elements using swapping. 
+
+  Note:- For more nuderstanding Go to(https://takeuforward.org/data-structure/insertion-sort-algorithm/)
+ * 
+ */
