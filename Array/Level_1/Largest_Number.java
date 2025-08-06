634b0c760f1fde84b93ac3471883e8b2f7306011
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-06 12:19:43 +0000

    Write code in array level problem find largest number in array

--- Array/Level_1/Largest_Number.java
+++ Array/Level_1/Largest_Number.java
@@ -1,5 +1,19 @@
-package Array;
+package Array.Level_1;
 
 public class Largest_Number {
     
+    /*
+     * in this problem we have to find the larh=gest number in the array
+     */
+    public static void main(String[] args) {
+        int[] arr ={15,18,56,84,3,6,7};
+        
+          int largest = arr[0];
+        for(int i =0; i< arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        System.out.println("Largest number in the array is" +largest);
+    }
 }
