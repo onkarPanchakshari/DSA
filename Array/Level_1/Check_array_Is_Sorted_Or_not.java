b04712fb38cd52bdb6c463a6c7f40c32d660fb60
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-06 13:26:59 +0000

    Solve the question of the Array is sorted or not

--- Array/Level_1/Check_array_Is_Sorted_Or_not.java
+++ Array/Level_1/Check_array_Is_Sorted_Or_not.java
@@ -1,5 +1,20 @@
 package Array.Level_1;
 
 public class Check_array_Is_Sorted_Or_not {
-    
+    public static boolean Check(int arr[]){
+
+        for( int i = 1; i< arr.length;i++){
+            if(arr[i] >= arr[i-1]){
+
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={ 1,2,2,3,3,4,5,9,11};
+        System.out.println("Array is shored or not:-" + Check(arr));
+    }
 }
