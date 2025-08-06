08a659b6924be7b439a6713f52cf281dff903c4c
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-06 12:59:38 +0000

    Solve secound smallest question

--- Array/Level_1/Second_Smallest_number.java
+++ Array/Level_1/Second_Smallest_number.java
@@ -1,5 +1,30 @@
 package Array.Level_1;
 
 public class Second_Smallest_number {
+
+    
+    public static int Ssmallest(int arr[]){
+        int Smallest = arr[0];
+        int sSmallest = -1;
+
+        for(int i = 0 ; i< arr.length;i++){
+            if(arr[i] < Smallest){
+                Smallest = arr[i];
+            }else if(arr[i] != sSmallest && arr[i] < Smallest)
+            {
+                sSmallest = arr[i];
+            }
+        }
+        
+        return sSmallest;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr ={15,18,56,84,3,6,7,7};
+        System.out.println("Second Smallest number in the array is :-"+ Ssmallest(arr) );
+         //this is optimal solution and time complexity of optimal solution is O(n)
+    
+    }
     
 }
