847699e84cb2f209030411747a7c8007f0493998
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-06 12:55:31 +0000

    Solve the smallest number in the array

--- Array/Level_1/Smallest_Number.java
+++ Array/Level_1/Smallest_Number.java
@@ -1,5 +1,15 @@
 package Array.Level_1;
 
 public class Smallest_Number {
-    
+     public static void main(String[] args) {
+        int[] arr ={15,18,56,84,3,6,7};
+        
+          int smallest = arr[0];
+        for(int i =0; i< arr.length;i++){
+            if(arr[i] < smallest){
+                smallest=arr[i];
+            }
+        }
+        System.out.println("Smallest number in the array is :- " + smallest);
+    }
 }
