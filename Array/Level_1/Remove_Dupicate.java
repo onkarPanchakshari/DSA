a44485c9fdff25adfc751466ee3b6d7683b814d0
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-11 08:13:07 +0000

    Add code to remove duplicate in array in array level 1

--- Array/Level_1/Remove_Dupicate.java
+++ Array/Level_1/Remove_Dupicate.java
@@ -1,5 +1,25 @@
 package Array.Level_1;
 
+import java.util.Arrays;
+
 public class Remove_Dupicate {
+
+    public static int removeDupicate(int[] arr){
+        int i = 0;
+        for( int j = 1; j < arr.length; j++){
+            if( arr[i] != arr[j]){
+                i++;
+                arr[i] =arr[j];
+            }
+        }
+        return i +1;
+    }
     
+    public static void main(String[] args) {
+        int[] arr ={1,1,2,2,2,3,3};
+        System.out.println(Arrays.toString(arr));
+        int s = removeDupicate(arr);
+        System.out.println(s);
+
+    }
 }
