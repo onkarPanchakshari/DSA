25a2106352ab9d6998259c17c019b9d96d6359cb
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-15 10:46:03 +0000

    Add the problem

---
+++ Array/Level_1/Left_Rotate_an_array_by_K_places.java
@@ -0,0 +1,29 @@
+package Array.Level_1;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Left_Rotate_an_array_by_K_places {
+
+    /*  
+    
+    Rotate array by K elements
+    Problem Statement: Given an array of integers, rotating array of elements by k elements either left or right.
+
+     */
+
+    public static void Rotate(int[] arr){
+
+
+    } 
+
+
+    public static void main(String[] args) {
+        int[] arr = {};
+        System.out.println(Arrays.toString(arr));
+        Rotate(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+    
+}
