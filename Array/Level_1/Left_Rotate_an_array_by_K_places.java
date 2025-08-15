9fd3b69002bbaad15e3cf469d3961f3ccaa6e07c
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-15 11:44:55 +0000

    Solve the question

--- Array/Level_1/Left_Rotate_an_array_by_K_places.java
+++ Array/Level_1/Left_Rotate_an_array_by_K_places.java
@@ -12,16 +12,32 @@ public class Left_Rotate_an_array_by_K_places {
 
      */
 
-    public static void Rotate(int[] arr){
-
+    public static void Rotate(int[] arr,int K){
+        int n = arr.length ;
+       if (n == 0)
+      return;
+    K = K % n;
+    if (K > n)
+      return;
+    int[] temp = new int[K];
+        for (int i = 0; i < K; i++) {
+      temp[i] = arr[i];
+    }
+    for (int i = 0; i < n - K; i++) {
+      arr[i] = arr[i + K];
+    }
+    for (int i = n - K; i < n; i++) {
+      arr[i] = temp[i - n + K];
+    }
 
     } 
 
 
     public static void main(String[] args) {
-        int[] arr = {};
+        int[] arr = {1,2,3,4,5,6};
+        int k = 2;
         System.out.println(Arrays.toString(arr));
-        Rotate(arr);
+        Rotate(arr,k);
         System.out.println(Arrays.toString(arr));
         
     }
