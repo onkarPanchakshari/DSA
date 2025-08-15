587009d443e371f25bdf1e0f35e148aa1e5b09af
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-15 10:41:31 +0000

    Solve the question

--- Array/Level_1/Left_Rotate_Arrray_By_One_Place.java
+++ Array/Level_1/Left_Rotate_Arrray_By_One_Place.java
@@ -1,8 +1,26 @@
 package Array.Level_1;
 
-public class Rotated_Arrray_By_One_Place {
+import java.util.Arrays;
+
+/*  Problem Statement: Given an array of 1 integers, left rotate the array by one place.
+
+*/
+
+public class Left_Rotate_Arrray_By_One_Place {
     
+
+    public static void Rotate(int[] arr){
+        int temp = arr[0];
+        for(int i = 1; i < arr.length;i++){
+            arr[i-1] = arr[i];
+        }
+        arr[arr.length-1] = temp;
+
+    }
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        Rotate(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
