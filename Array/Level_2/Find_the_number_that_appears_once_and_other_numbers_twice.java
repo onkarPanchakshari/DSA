e9d643461a2c23fb2af6f6aa83149f0a1f518a50
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-18 14:24:36 +0000

    solve the problem

--- Array/Level_2/Find_the_number_that_appears_once_and_other_numbers_twice.java
+++ Array/Level_2/Find_the_number_that_appears_once_and_other_numbers_twice.java
@@ -2,4 +2,18 @@ package Array.Level_2;
 
 public class Find_the_number_that_appears_once_and_other_numbers_twice {
     
+    public static int getSingleElement(int[] arr){
+        int xorr = 0;
+        for(int i =0 ;i<arr.length;i++){
+            xorr = xorr ^ arr[i];
+        }
+        return xorr;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+    }
 }
