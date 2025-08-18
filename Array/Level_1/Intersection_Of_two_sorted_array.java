3886b78e56d4551f100804b6431344e2c512aa2d
onkarPanchakshari <157611034+onkarPanchakshari@users.noreply.github.com>
2025-08-18 13:45:36 +0000

    add the code

--- Array/Level_1/Intersection_Of_two_sorted_array.java
+++ Array/Level_1/Intersection_Of_two_sorted_array.java
@@ -1,5 +1,6 @@
 package Array.Level_1;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /*
@@ -8,6 +9,27 @@ import java.util.ArrayList;
  */
 public class Intersection_Of_two_sorted_array {
 
+
+    public static ArrayList<Integer> FindInsertion(int[] arr1,int[] arr2){
+       int m = arr1.length;
+       int n = arr2.length;
+       int i =0,j=0;
+        ArrayList<Integer> insertion = new ArrayList<>();
+
+        while(i<m && j<n){
+            if(arr1[i] < arr2[j]){
+                i++;
+            }else if (arr2[j] < arr1[i]) {
+                j++;
+            }else{
+                insertion.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+       
+        return insertion;
+    }
 
 
     public static void main(String[] args) {
